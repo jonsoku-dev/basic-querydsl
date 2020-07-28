3d3c1549f58787d0802f3dfd55c6b89fa06868b5
jongseok-lee <jongseok.lee@linecorp.com>
2020-07-28 18:45:31 +0900

    MemberRepository추가

---
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package study.querydsl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsl.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
