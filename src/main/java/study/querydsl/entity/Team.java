488cbfe62388f31176c1adf846f2a14633849f2f
jongseok-lee <jongseok.lee@linecorp.com>
2020-07-28 16:47:23 +0900

    Member & Team Entity 추가 및 테스트케이스 작성

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,32 @@
+package study.querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {})
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
