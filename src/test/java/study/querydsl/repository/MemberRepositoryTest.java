3260c7bf04ef60c799cdfddf2c2a9e2eeccc88da
jongseok-lee <jongseok.lee@linecorp.com>
2020-07-28 19:07:14 +0900

    member repository test case 작성

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,6 +1,5 @@
 package study.querydsl.repository;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
